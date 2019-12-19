43262db5fa4ab9dc94a82e90894ac83697ed1080
moussaab moulim <moussaabmma@gmail.com>
2019-12-19 23:43:41 +0000

    adding security parameters interface for jwt parameters

---
+++ src/main/java/com/moussaabmma/microsrvsec/security/SecurityParams.java
@@ -0,0 +1,8 @@
+package com.moussaabmma.microsrvsec.security;
+
+public interface SecurityParams {
+    public static final String JWT_HEADER_NAME="Authorization";
+    public static final String SECRET="moussaabmoulim.ml";
+    public static final long EXPIRATION=10*24*3600;
+    public static final String HEADER_PREFIX="Bearer ";
+}
