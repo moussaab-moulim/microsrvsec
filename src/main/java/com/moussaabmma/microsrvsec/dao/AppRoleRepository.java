d6c59e064d1f77b24576940196e4be749f342d23
moussaab moulim <moussaabmma@gmail.com>
2019-12-19 00:38:50 +0000

    creating Role jpa repository

---
+++ src/main/java/com/moussaabmma/microsrvsec/dao/AppRoleRepository.java
@@ -0,0 +1,11 @@
+package com.moussaabmma.microsrvsec.dao;
+
+
+import com.moussaabmma.microsrvsec.entities.AppRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface AppRoleRepository extends JpaRepository<AppRole,Long> {
+    public AppRole findByRoleName(String roleName);
+}
