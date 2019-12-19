9aafa4224c2841220c0d0703953846b1584b739f
moussaab moulim <moussaabmma@gmail.com>
2019-12-19 00:43:58 +0000

    adding mysql connector and updating the project configurations

---
+++ src/test/java/com/moussaabmma/microsrvsec/MicrosrvsecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moussaabmma.microsrvsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosrvsecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
